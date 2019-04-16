7b545b85e9e2a7ff321dad0741d445e7ab51e353
koshikraj <koshik.raj@gmail.com>
2019-04-16 16:51:47 +0530

    adds rinkeby network details

--- truffle.js
+++ truffle.js
@@ -1,12 +1,27 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+// require('babel-register');
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = 'crouch dry film picnic music noise fiber loud candy match leader coffee';
+
 
 module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // Match any network id
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        rinkeby: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/c0201f3cd3894e30b62af4bb542b5779');
+                    },
+            network_id: 4,
+            gas: 4500000,
+            gasPrice: 10000000000,
+        },
+
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*' // Match any network id
+        }
     }
-  }
-}
+};
